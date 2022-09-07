f41719814b4bd09be886c7e2ced062b164c09fcf
a708218 <abderrazak.mifdal@worldline.com>
2022-09-07 09:55:12 +0200

    Fix : add or update event review

--- src/main/java/adeo/leroymerlin/cdp/EventRepository.java
+++ src/main/java/adeo/leroymerlin/cdp/EventRepository.java
@@ -1,6 +1,9 @@
 package adeo.leroymerlin.cdp;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -11,4 +14,9 @@ public interface EventRepository extends Repository<Event, Long> {
     void deleteById(Long eventId);
 
     List<Event> findAllBy();
+
+    @Modifying
+    @Query("update Event e set e.nbStars = :nbStars, e.comment = :comment where e.id = :id")
+    void updateReview(@Param(value = "id") long id, @Param(value = "nbStars") Integer nbStars, @Param(value = "comment") String comment);
+
 }
