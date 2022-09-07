d5a1bb68a9c9ea321096ed3467f9312b12baa66f
a708218 <abderrazak.mifdal@worldline.com>
2022-09-07 15:33:08 +0200

    Feat : Filter events which has at least one band with a member name matching with query pattern

--- src/main/java/adeo/leroymerlin/cdp/EventService.java
+++ src/main/java/adeo/leroymerlin/cdp/EventService.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class EventService {
@@ -27,10 +29,26 @@ public class EventService {
         List<Event> events = eventRepository.findAllBy();
         // Filter the events list in pure JAVA here
 
+        // Filter events which has at least one band with a member name matching with query pattern
+        events = events.stream()
+                .filter(event ->
+                        (event.getBands().stream()
+                                .filter(band -> band.getMembers()
+                                        .removeIf(member -> !matchWithPattern(member.getName(), query))).count() > 0) // Delete all members witch not match with pattern
+                                && event.getBands().removeIf(band -> band.getMembers().size() == 0) // Remove band with no member
+                                && event.getBands().size() > 0) // Filter event with no band
+                .toList();
+
         return events;
     }
 
     public void updateReview(Long id, Event event) {
         eventRepository.updateReview(id, event.getNbStars(), event.getComment());
+    }
+
+    private boolean matchWithPattern(String memberName, String query) {
+        Pattern p = Pattern.compile(query.toLowerCase());
+        Matcher m = p.matcher(memberName.toLowerCase());
+        return m.find();
     }
 }
